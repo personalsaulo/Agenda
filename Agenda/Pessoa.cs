c238a54b9c90318e4f156445d85771cb85f7a7ee
saulo.carvalho <saulo.carvalho@SJ215V04-2T2018>
2018-05-25 21:32:57 -0300

    Agenda simples que cadastra e pesquisa.

---
+++ Agenda/Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agenda
+{
+    public class Pessoa
+    {
+        public string  Email { get; set; }
+        public int Idade { get; set; }
+        public string Nome { get; set; }
+        List<Pessoa> listaPessoa = new List<Pessoa>();
+
+        public void cadastraPessoa(Pessoa pessoa)
+        {
+            listaPessoa.Add(pessoa);
+        }
+
+        public void BuscaPessoa(string nome)
+        {
+            for(int i=0; i < listaPessoa.Count; i++)
+            {
+                if (listaPessoa[i].Nome.Contains(nome))
+                {
+                    Console.WriteLine(" Nome: " + listaPessoa[i].Nome + " Idade: "+ listaPessoa[i].Idade + " Email: " + listaPessoa[i].Email);
+                }
+
+            }
+        }
+    }
+}
--- Agenda/Program.cs
+++ Agenda/Program.cs
@@ -6,7 +6,46 @@ namespace Agenda
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcao = 0;
+            Pessoa pessoa = new Pessoa();
+            do
+            {
+                Console.WriteLine("Agenda");
+                Console.WriteLine("1 - Cadastrar");
+                Console.WriteLine("2 - Pesquisar");
+                Console.WriteLine("3 - Sair");
+
+                opcao = int.Parse(Console.ReadLine());
+
+
+                if (opcao == 1)
+                {
+                    Console.WriteLine("Cadastrando....: ");
+                    Console.WriteLine("Nome: ");
+                    pessoa.Nome = Console.ReadLine();
+                    Console.WriteLine("Idade: ");
+                    pessoa.Idade = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Email: ");
+                    pessoa.Email = Console.ReadLine();
+
+                    pessoa.cadastraPessoa(pessoa);
+                }
+
+                if (opcao == 2)
+                {
+                    Console.WriteLine("Pesquisando.... ");
+                    Console.WriteLine("Nome: ");
+                    string nome = Console.ReadLine();
+
+                    pessoa.BuscaPessoa(nome);
+                }
+                    
+
+
+            } while (opcao != 3 );
+
+            Console.ReadKey();
+
         }
     }
 }
